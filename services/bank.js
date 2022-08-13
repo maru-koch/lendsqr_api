7584f2ebd32132def947c99f3ea0095a899b2f63
maru-koch <nwokochafranklyn@gmail.com>
2022-08-13 13:08:09 +0100

    feat: user can create account
    implemented the business logic to auto-generate account number

--- services/bank.js
+++ services/bank.js
@@ -1,13 +1,19 @@
 
 class Bank{
-    
+
     // a class that perform all bank related operations
 
     constructor(bankCode){
         this.bankCode = bankCode;
     }
-
+    
     generateAccountNumber(){
-
+        const nuban = 10;
+        const accountNumber = '';
+        for (let i = 0; i < nuban; i++){
+            const digit = Math.random(1, 9);
+            accountNumber += digit.toString()
+        }
+        return accountNumber
     }
 }
