972add28f78dc95042ab1a9f4019aaa9793c5b9a
maru-koch <nwokochafranklyn@gmail.com>
2022-08-13 20:49:35 +0100

    added a middleware for the user id validation

--- routes/accountRouter.js
+++ routes/accountRouter.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.router()
 const account = require('../controllers/accountController')
+const { isValidID } = require('../middlewares/accountMiddleware')
 
 router.post('/create', account.create)
-router.get('/getDetails', account.getDetails)
+router.get('/getDetails', isValidID, account.getDetails)
 
 module.exports = router
